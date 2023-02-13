df08017cff1ccb7bd0647fae279a4032b58ad754
Sasit Srirat <55717534+sasitsrirat@users.noreply.github.com>
2023-02-13 21:31:13 +0700

    Update App.java

--- exercise4_6413112/ex4/src/main/java/Ex4_6413112/App.java
+++ exercise4_6413112/ex4/src/main/java/Ex4_6413112/App.java
@@ -1,6 +1,8 @@
 package Ex4_6413112;
 
 
+import java.util.ArrayDeque;
+import java.util.Comparator;
 import java.util.PriorityQueue;
 import java.util.Random;
 import java.util.Scanner;
@@ -20,48 +22,81 @@ class Customer {
         this.priority = priority;
         this.seats = seats;
     }
+    public int getID()
+    {
+        return ID;
+    };
+    public int priority()
+    {
+        return priority;
+    };
+    public int seats()
+    {
+        return seats;
+    };
+    public void print() {
+        System.out.printf("%d  %d %d \n", ID, priority, seats);
+    }
 }
 
 class Restaurant {
     private int maxSeats, maxTime; // from user input
-    //private PriorityQueue<Customer> waitingQueue; // sorted by priority, then by ID
-    //private ArrayDeque<Customer> diningQueue;
-    public void simulation() { 
+    public  PriorityQueue<Customer> waitingQueue = new PriorityQueue<Customer>(); // sorted by priority, then by ID
+    private ArrayDeque<Customer> diningQueue;
+    public int seatreq,reqseat;
+    Random rand = new Random();
+ 
+    public void simulation(int maxSeats,int maxTime) { 
         /* implement restaurant simulation */ 
-
-
         
+        for (int t = 1; t <= maxTime ; t++) //change it to MaxTime
+        {
+            //create a new Customer with random priority & random seats (1-10). put new customer in waitingQueue
+                int Random_p = rand.nextInt(3) + 1; //random priority 
+                int Random_s = rand.nextInt(10) + 1; //random seat 
+                waitingQueue.add(new Customer(t, t, Random_s));
+                System.out.printf("[Customer %d, normal, books  %d seats]\n",waitingQueue);
+
+        }
+
+        //System.out.println("New arrival >> ");
+        //System.out.printf("\nSize 2 = %d \n", waitingQueue.size());
+        for (int j = 1; j <= 2; j++)
+        {   
+            //System.out.println("Customer to dine %d >> ");
+            //System.out.printf("[Customer %d, normal, books  %d seats]\n",waitingQueue);
+
+        }
     }
 }
 
-public class App 
+class SortByPriority implements Comparator<Customer>
 {
-    public static void main( String[] args )
+
+    @Override
+    public int compare(Customer o1, Customer o2) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+}
+
+public class Process
+{
+    public static void main(String[] args)
     {
         Scanner sc1 = new Scanner(System.in);
         Scanner sc2 = new Scanner(System.in);
-        Random rand = new Random();
+        Restaurant r = new Restaurant();
 
         System.out.println("Enter max seats = ");
         int maxSeats = sc1.nextInt();
         System.out.println("Enter max time = ");
         int maxTime = sc2.nextInt();
-
         System.out.println("=== Customers already in queue ===");
-        
-        //create a new Customer with random priority & random seats (1-10). put new customer in waitingQueue
-        for (int i = 1; i <=5 ; i++)
-        {
-            int Random = rand.nextInt(10) + 1;
-            System.out.printf("[Customer %d, normal, books  %d seats]\n",i,Random);
-
-        }
-
         System.out.println("=== Simulation ===");
-        //System.out.println("New arrival >> ");
-        
-       
-        
-        
+        r.simulation(maxSeats,maxTime);
+ 
     }
 }
+
