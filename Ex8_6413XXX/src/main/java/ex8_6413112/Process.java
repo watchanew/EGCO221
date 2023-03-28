64f7a3c39889202f60a42365fbd7b8540e78c65f
Sasit Srirat <55717534+sasitsrirat@users.noreply.github.com>
2023-03-28 13:41:39 +0700

    Update Process.java

--- Ex8_6413XXX/src/main/java/ex8_6413112/Process.java
+++ Ex8_6413XXX/src/main/java/ex8_6413112/Process.java
@@ -7,25 +7,24 @@ package ex8_6413112;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.*;
- 
 
 class FlagFeatureMap {
     // Key = feature, Value = set of countries
-    private TreeMap<String, LinkedHashSet<String>> workingMap; //String,Integer
+    private TreeMap<String, LinkedHashSet<String>> workingMap; // String,Integer
     private LinkedHashSet<String> allCountries;
-    private ArrayList<String> A ;
-    
+    private ArrayList<String> A;
+
     public FlagFeatureMap() // constructor
     {
         workingMap = new TreeMap<String, LinkedHashSet<String>>();
         allCountries = new LinkedHashSet<String>();
     }
-    public void addCountry(String aCT)
-    {
+
+    public void addCountry(String aCT) {
         allCountries.add(aCT);
     }
-    public void addFlag(String aCT, ArrayList<String> A)
-    {
+
+    public void addFlag(String aCT, ArrayList<String> A) {
         for (String i : A) {
             if (workingMap.containsKey(i)) {
                 workingMap.get(i).add(aCT);
@@ -35,38 +34,46 @@ class FlagFeatureMap {
             }
         }
     }
-    public void includeCountries(ArrayList<String> wantedFeatures) {
-        // Get set of countries using each wanted feature as a key
-        // Use set operations to include countries whose flags have wanted features
 
-        ArrayList<String> A = new ArrayList<>();
-        if(wantedFeatures.isEmpty())
-        {
-            System.out.print("{}");
-            for (String w:workingMap.keySet()) allCountries.add(w);
-            A.add("{}");
-        }
-        else
-            {
-                for (int i = 0; i < wantedFeatures.size(); i++)
-                {
-                    //for (String a : allFeature)
+    public void includeCountries(ArrayList<String> wantedFeatures) {
+        System.out.println("\n");
+        ArrayList<String> countryList = new ArrayList<>();
+        if (wantedFeatures.isEmpty()) {
+            for (LinkedHashSet<String> featureCountries : workingMap.values()) {
+                if (featureCountries != null) {
+                    countryList.addAll(featureCountries);
                 }
             }
+        } else {
+            for (String feature : wantedFeatures) {
+                LinkedHashSet<String> featureCountries = workingMap.get(feature);
+                if (featureCountries != null) {
+                    countryList.addAll(featureCountries);
+                }
+            }
+        }
+        Collections.sort(countryList);
+        for (int i = 0; i < countryList.size(); i++) {
+            if (i % 7 == 0) {
+                System.out.printf("\n                  ");
+            }
+            System.out.printf("%-15s  ", countryList.get(i));
+        }
+        System.out.println();
     }
 
     public void excludeCountries(ArrayList<String> unwantedFeatures) {
         // Get set of countries using each unwanted feature as a key
         // Use set operations to exclude countries whose flags have unwanted features
+        System.out.printf("\n");
+        ArrayList<String> T = new ArrayList<String>();
     }
 
-    public void printData()
-    {
+    public void printData() {
         System.out.println("Total Countries = " + allCountries.size());
         System.out.println("Total Features = " + workingMap.size());
         Set<String> OutKey = workingMap.keySet();
-        for(String key : OutKey)
-        {
+        for (String key : OutKey) {
             LinkedHashSet<String> s = workingMap.get(key);
             if (s != null) {
                 System.out.printf("%12s  >>  ", key);
@@ -89,46 +96,43 @@ class FlagFeatureMap {
         }
     }
 }
-public class Process
-{
-    //Write a main class to do the following:
-    //2.1 Read data from flags.txt into workingMap and allCountries.
-    public static void main (String[] args)
-    {
+
+public class Process {
+    // Write a main class to do the following:
+    // 2.1 Read data from flags.txt into workingMap and allCountries.
+    public static void main(String[] args) {
         FlagFeatureMap F = new FlagFeatureMap();
         ArrayList<String> A = new ArrayList<String>();
 
         String filename = "flags.txt";
 
-            try{
-                TreeSet<String> allFlag = new TreeSet<String>();
-                LinkedHashSet<String> flag = new LinkedHashSet<String>();
+        try {
+            TreeSet<String> allFlag = new TreeSet<String>();
+            LinkedHashSet<String> flag = new LinkedHashSet<String>();
 
-                Scanner filescan = new Scanner(new File(filename));
-                //String line = file.nextLine();
-                while(filescan.hasNextLine())
-                {
-                    String buf[] = filescan.nextLine().split(",");
-                    String last = buf[buf.length - 1];
-                    F.addCountry(buf[0].trim());
-                    //System.out.println(last); //show colour,flag
-                    //System.out.println(buf[0].trim()); //show Countries
-                    //System.out.println(filescan.nextLine()); //printall
-                    for(int i = 1 ; i < buf.length;i++)
-                    {
-                        A.add(buf[i].trim());
-                        //System.out.println(buf[i].trim()); //flag,country
-                        //allFlag.add(buf[i]);
-                        //flag.add(buf[i]);
-                    }
-                    F.addFlag(buf[0].trim(),A);
-                    A.clear();
+            Scanner filescan = new Scanner(new File(filename));
+            // String line = file.nextLine();
+            while (filescan.hasNextLine()) {
+                String buf[] = filescan.nextLine().split(",");
+                String last = buf[buf.length - 1];
+                F.addCountry(buf[0].trim());
+                // System.out.println(last); //show colour,flag
+                // System.out.println(buf[0].trim()); //show Countries
+                // System.out.println(filescan.nextLine()); //printall
+                for (int i = 1; i < buf.length; i++) {
+                    A.add(buf[i].trim());
+                    // System.out.println(buf[i].trim()); //flag,country
+                    // allFlag.add(buf[i]);
+                    // flag.add(buf[i]);
                 }
-            }catch(FileNotFoundException e)
-            {
-                System.out.println(e);
+                F.addFlag(buf[0].trim(), A);
+                A.clear();
             }
-            F.printData();
+        } catch (FileNotFoundException e) {
+            System.out.println(e);
+        }
+        F.printData();
+
     }
-    
+
 }
